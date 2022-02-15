6c51ca3dfdeb3707be21c4f7f54846dada4646a7
Tom Barron <tusculum@gmail.com>
2022-02-15 17:26:27 -0500

    Omit css update if not defined

--- home/index.js
+++ home/index.js
@@ -1,5 +1,5 @@
 window.onload = show_update;
-var js_update = "Updated: <2022.0214 12:35:37>";
+var js_update = "Updated: <2022.0215 17:26:02>";
 
 function clean(s) {
     let tmp = s.replace("Updated: ", "");
@@ -11,6 +11,8 @@ function clean(s) {
 }
 
 function show_update() {
+    let css_upd;
+
     // HTML update
     let html = document.getElementById("updated");
     let html_upd = (html.innerHTML !== null) ? html.innerHTML : html.value;
@@ -18,16 +20,31 @@ function show_update() {
 
     // CSS update
     let css = document.getElementsByClassName("cssmeta");
-    let style = window.getComputedStyle(css[0], null);
-    let css_upd = clean(style.getPropertyValue("--updated"));
+    if (0 < css.length) {
+        console.log("cssmeta items found: ", css.length);
+        let style = window.getComputedStyle(css[0], null);
+        css_upd = clean(style.getPropertyValue("--updated"));
+    } else {
+        css_upd = "";
+    }
 
     // JS update
     let js_upd = clean(js_update);
 
-    let result = "HTML: " + html_upd +
-        "<br>CSS: " + css_upd +
-        "<br>JS: " + js_upd;
-    
+    let result = "";
+
+    if ("" != html_upd) {
+        result += "HTML: " + html_upd;
+    }
+
+    if ("" != css_upd) {
+        result += "<br>CSS: " + css_upd;
+    }
+
+    if ("" != js_upd) {
+        result += "<br>JS: " + js_upd;
+    }
+
     let target = document.getElementById("last_update");
     if (target !== null) {
         console.log(target);
