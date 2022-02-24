db791505f622558b5f1f34d11576e0d942589928
Tom Barron <tusculum@gmail.com>
2022-02-24 06:39:59 -0500

    Javascript cleanup: remove unused functions, ...
    put those remaining in a sensible order and document them

--- prj/cpp/cpp.js
+++ prj/cpp/cpp.js
@@ -1,44 +1,27 @@
-var js_update = "Updated: <2022.0223 20:10:04>"
+var js_update = "Updated: <2022.0224 06:37:48>"
 
-function setElementValue(id, val) {
-    var el = document.getElementById(id);
-    el.value = val;
-    el.innerHTML = val;
-}
+/* ----------------------------------------------------------------------
+ * Manage calling pkg_calculation. This function is called from the
+ * HTML.
+ */
+function pkg_compute() {
+    var cps = getFloatValue("cps");
+    var spp = getFloatValue("spp");
+    var wpp = getFloatValue("wpp");
+    var unit_pp = getFloatValue("unit_pp");
 
-function setFocus(id) {
-    var el = document.getElementById(id);
-    el.focus();
-}
-
-function cal_by_weight(cal_p_unit, unit_mult) {
-    return(cal_p_unit * unit_mult);
-}
-
-function clear_elements(elist) {
-    for (var idx = 0 ; idx < elist.length ; idx++) {
-        setElementValue(elist[idx], "");
+    if (isNaN(cps) || isNaN(spp) || isNaN(wpp)) {
+        return;
     }
+
+    var result = pkg_calculation(cps, spp, wpp, unit_pp);
+
+    setElementValue("cpp", result);
 }
 
-function instruct() {
-    msg = "Please fill in the first three fields or"
-        + " the cal per ounce/gram field with numbers";
-    alert(msg);
-}
-
-function request_spp() {
-    alert("Please fill in servings per package");
-}
-
-function request_wpp() {
-    alert("Please fill in ounces/grams per package");
-}
-
-function hell_frozen() {
-    alert("Hell has frozen over");
-}
-
+/* ----------------------------------------------------------------------
+ * Calculate a result based on the packaging fields.
+ */
 function pkg_calculation(cps, spp, wpp, unit_pp) {
     if (isNaN(spp)) {
         request_spp();
@@ -47,19 +30,98 @@ function pkg_calculation(cps, spp, wpp, unit_pp) {
     } else {
         var cal_p_pkg = cps * spp;
         var lb_p_pkg = wpp / unit_pp;
-        // setElementValue(cpp_id, cal_p_pkg / lb_p_pkg);
-        return cal_p_pkg / lb_p_pkg;
+        return Math.round(cal_p_pkg / lb_p_pkg);
     }
 }
 
+/* ----------------------------------------------------------------------
+ * When focus leaves the "calories per ounce/gram" field, compute the 
+ * result.
+ */
+function groz_compute() {
+    var cpw = getFloatValue("cpw");
+    var cp_unit = getFloatValue("cp_unit");
+
+    if (!isNaN(cpw)) {
+        result = wgt_calculation(cpw, cp_unit);
+        setElementValue("cpp", result);
+    }
+
+    setFocus("cps");
+}
+
+/* ----------------------------------------------------------------------
+ * Calculate a result based on the weight field.
+ */
 function wgt_calculation(cpw, cp_unit) {
-    return cpw * cp_unit;
+    return Math.round(cpw * cp_unit);
 }
 
-function click_clear() {
-    clear_elements(["cps", "spp", "wpp", "cpw", "cpp"]);
+/* ----------------------------------------------------------------------
+ * After the "per package" ounce/grams selector is manipulated, we
+ * want the focus to go the to the nearest field.
+ */
+function pkg_sel_done() {
+    setFocus("wpp");
 }
 
+/* ----------------------------------------------------------------------
+ * After the "calories per" ounce/grams selector is manipulated, we 
+ * want the focus to go the nearest field.
+ */
+function groz_sel_done() {
+    setFocus("cpw");
+}
+
+/* ----------------------------------------------------------------------
+ * If we try to recalculate when the spp field is NaN, ask the user to
+ * respond usefully.
+ */
+function request_spp() {
+    alert("Please fill in servings per package");
+}
+
+/* ----------------------------------------------------------------------
+ * If we try to recalculate when the wpp field is NaN, ask the user to
+ * respond usefully.
+ */
+function request_wpp() {
+    alert("Please fill in ounces/grams per package");
+}
+
+/* ----------------------------------------------------------------------
+ * Clear all visible fields
+ */
+function resetFields() {
+    setElementValue("cps", "");
+    setElementValue("spp", "");
+    setElementValue("wpp", "");
+    setElementValue("cpw", "");
+    setElementValue("cpp", "");
+    setFocus("cps");
+}
+
+/* ----------------------------------------------------------------------
+ * Given an element *id*, set that element to *value*.
+ */
+function setElementValue(id, val) {
+    var el = document.getElementById(id);
+    el.value = val;
+    el.innerHTML = val;
+}
+
+/* ----------------------------------------------------------------------
+ * Given an element *id*, set the focus to that element.
+ */
+function setFocus(id) {
+    var el = document.getElementById(id);
+    el.focus();
+}
+
+/* ----------------------------------------------------------------------
+ * Given an element id *element*, retrieve its value and parse it as a
+ * float.
+ */
 function getFloatValue(element) {
     var el = document.getElementById(element);
     var zz;
@@ -75,68 +137,3 @@ function getFloatValue(element) {
     return rv;
 }
 
-function click_calculate() {
-    var cps = getFloatValue("cps");
-    var cpw = getFloatValue("cpw");
-    var spp = getFloatValue("spp");
-    var wpp = getFloatValue("wpp");
-    var cpw = getFloatValue("cpw");
-    var unit_pp = getFloatValue("unit_pp");
-    var cp_unit = getFloatValue("cp_unit");
-
-    if (isNaN(cps) && isNaN(cpw)) {
-        instruct();
-    } else if (!isNaN(cps)) {
-        pkg_calculation(cps, spp, wpp, unit_pp, "cpp");
-    } else if (!isNaN(cpw)) {
-        wgt_calculation(cpw, cp_unit, "cpp");
-    } else {
-        hell_frozen();
-    }
-}
-
-function pkg_sel_done() {
-    var el = document.getElementById("wpp");
-    el.focus();
-}
-
-function groz_sel_done() {
-    var el = document.getElementById("cpw");
-    el.focus();
-}
-
-function groz_compute() {
-    var cpw = getFloatValue("cpw");
-    var cp_unit = getFloatValue("cp_unit");
-
-    if (!isNaN(cpw)) {
-        result = wgt_calculation(cpw, cp_unit);
-        setElementValue("cpp", result);
-    }
-
-    setFocus("cps");
-}
-
-function pkg_compute() {
-    var cps = getFloatValue("cps");
-    var spp = getFloatValue("spp");
-    var wpp = getFloatValue("wpp");
-    var unit_pp = getFloatValue("unit_pp");
-    
-    if (isNaN(cps) || isNaN(spp) || isNaN(wpp)) {
-        return;
-    }
-
-    var result = pkg_calculation(cps, spp, wpp, unit_pp);
-
-    setElementValue("cpp", result);
-}
-
-function resetFields() {
-    setElementValue("cps", "");
-    setElementValue("spp", "");
-    setElementValue("wpp", "");
-    setElementValue("cpw", "");
-    setElementValue("cpp", "");
-    setFocus("cps");
-}
