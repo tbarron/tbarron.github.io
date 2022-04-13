893edaae4fe8ee6134ef4ed1a1a1a22eb7b8dd1b
Tom Barron <tusculum@gmail.com>
2022-04-13 13:47:18 -0400

    Enable setting the browser window to a solid color

--- prj/jsfx/jsfx.js
+++ prj/jsfx/jsfx.js
@@ -1,1 +1,17 @@
-var js_update = "Updated: <2022.0222 08:14:10>"
+var js_update = "Updated: <2022.0410 19:32:20>"
+
+function holdColor() {
+    var elm = document.getElementById("which_color");
+    console.log("element value: ", elm.value);
+
+    var body = document.getElementsByTagName("body")[0];
+    console.log(body);
+    body.style.background = elm.value;
+    body.setAttribute("onclick", "resetColor()");
+}
+
+function resetColor() {
+    console.log("resetColor: ...")
+    var body = document.getElementsByTagName("body")[0];
+    body.style.background = "lightyellow";
+}
